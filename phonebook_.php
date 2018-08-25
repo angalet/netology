9b92625ee61212e1956caf09b202b064efc83645
Angalet Bogdan <angaleit@gmail.com>
2018-08-25 20:54:56 +0300

    commit

--- phonebook_.php
+++ phonebook_.php
@@ -1,4 +1,11 @@
 <?php
-//чсмчсмчмчсмчмс
-//лилиоил
+$file = file_get_contents('json.txt');
+$file = json_decode($file, true);
+echo "<pre>";
+var_dump($file);
+echo "</pre>";
+foreach($file as $value){
+    echo $value["firstName"]." - ".$value["lastName"]." - ".$value["address"]." - ".$value["phoneNumber"]."<br>";
+}
+//echo $file[0]["firstName"];
 ?>
